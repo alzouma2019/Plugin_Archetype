b0632f6d6202f30af6643aa8e3a561a1d83e82a8
AdeyoleFr <Francesca.adeyole@yahoo.com>
2020-02-07 10:54:39 +0100

    Premier commit

---
+++ src/main/java/org/exemple/demo/App.java
@@ -0,0 +1,13 @@
+package org.exemple.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Projet de Alzouma Moussa Mahamadou , Etudiant en Master 2 Informatique 2019-2020: SMAGIR2A PIGIER COTE d'IVOIRE" );
+    }
+}
